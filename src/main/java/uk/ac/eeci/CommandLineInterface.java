d79ee3ed6d7cbb52dfff251763a46cc2d78fdd05
Tim Tröndle <tt397@cam.ac.uk>
2017-03-03 15:13:38 +0000

    Add simple command line interface

---
+++ src/main/java/uk/ac/eeci/CommandLineInterface.java
@@ -0,0 +1,47 @@
+package uk.ac.eeci;
+
+import io.improbable.scienceos.Conductor;
+import org.apache.commons.cli.*;
+
+public class CommandLineInterface {
+
+    private String inputFilePath;
+    private String outputFilePath;
+
+    public static void main(String ... args) {
+
+        Options options = new Options();
+
+        Option input = new Option("i", "input", true, "file path to scenario db");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("o", "output", true, "file path for output db");
+        output.setRequired(true);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("energy-agents", options);
+
+            System.exit(1);
+            return;
+        }
+
+        CommandLineInterface cli = new CommandLineInterface();
+        cli.inputFilePath = cmd.getOptionValue("input");
+        cli.outputFilePath = cmd.getOptionValue("output");
+        cli.run();
+    }
+
+    private void run() {
+        CitySimulation citySimulation = ScenarioBuilder.readScenario(this.inputFilePath, this.outputFilePath);
+        new Conductor(citySimulation).run();
+    }
+}
